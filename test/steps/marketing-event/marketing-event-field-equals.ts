17c0d56d7ab168e69e6b5973079c27c2e6b98d87
saludangelito <asalud@you-source.com>
2022-03-28 18:15:36 +0800

    updated unit tests

--- test/steps/marketing-event/marketing-event-field-equals.ts
+++ test/steps/marketing-event/marketing-event-field-equals.ts
@@ -31,7 +31,7 @@ describe('MarketingEventFieldEquals', () => {
       const stepDef: StepDefinition = stepUnderTest.getDefinition();
       expect(stepDef.getStepId()).to.equal('MarketingEventFieldEquals');
       expect(stepDef.getName()).to.equal('Check a field on a HubSpot Marketing Event');
-      expect(stepDef.getExpression()).to.equal('the (?<field>[a-zA-Z0-9_-]+) field on hubspot marketing external event id (?<externalEventId>.+\@.+\..+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)?');
+      expect(stepDef.getExpression()).to.equal('the (?<field>[a-zA-Z0-9_-]+) field on hubspot marketing external event id (?<externalEventId>.+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)?');
       expect(stepDef.getType()).to.equal(StepDefinition.Type.VALIDATION);
     });
 
