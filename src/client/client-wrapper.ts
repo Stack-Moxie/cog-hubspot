671f19aef92895e525d72b746b1026bc367407c0
Sev Geraskin <geraskin@gmail.com>
2023-08-04 07:40:39 -0700

    added V3 authentication

--- src/client/client-wrapper.ts
+++ src/client/client-wrapper.ts
@@ -1,5 +1,6 @@
 import * as grpc from 'grpc';
 import * as Hubspot from 'hubspot';
+import { Client } from '@hubspot/api-client';
 import { Field } from '../core/base-step';
 import { FieldDefinition } from '../proto/cog_pb';
 import {
@@ -14,6 +15,7 @@ import {
   QuoteAwareMixin,
   TicketAwareMixin,
   WorkflowAwareMixin,
+  ImportsAwareMixinV3,
 } from './mixins';
 
 class ClientWrapper {
@@ -24,9 +26,34 @@ class ClientWrapper {
   }];
 
   client: Hubspot.default;
+  clientV3: Client;
   clientReady: Promise<boolean>;
+  auth: grpc.Metadata;
+
+  connectToV3 = async () => {
+    if (this.auth.get('refreshToken').toString()) {
+      const result = await this.clientV3.oauth.tokensApi.create(
+        'refresh_token',
+        undefined,
+        undefined,
+        this.auth.get('clientId').toString(),
+        this.auth.get('clientSecret').toString(),
+        this.auth.get('refreshToken').toString(),
+      );
+      console.log('Updated refresh token', result);
+      // this assigns the accessToken to the client, so your client is ready
+      // to use
+      this.clientV3.setAccessToken(result.accessToken);
+      return;
+    }
+    // Fallback to Private App Api Token
+    this.clientV3 = new Client({
+      accessToken: this.auth.get('accessToken').toString(),
+    });
+  }
 
   constructor(auth: grpc.Metadata, clientConstructor = Hubspot.default) {
+    this.auth = auth;
     // Support OAuth-based authentication under the hood.
     if (auth.get('refreshToken').toString()) {
       this.client = new clientConstructor({
@@ -63,7 +90,8 @@ interface ClientWrapper extends
   MarketingEventAwareMixin,
   QuoteAwareMixin,
   AssociationAwareMixin,
-  ContactListAwareMixin { }
+  ContactListAwareMixin,
+  ImportsAwareMixinV3 { }
 
 applyMixins(ClientWrapper, [
   ContactAwareMixin,
@@ -77,6 +105,7 @@ applyMixins(ClientWrapper, [
   QuoteAwareMixin,
   AssociationAwareMixin,
   ContactListAwareMixin,
+  ImportsAwareMixinV3,
 ]);
 
 function applyMixins(derivedCtor: any, baseCtors: any[]) {
---
+++ src/client/mixins/imports-aware.ts
@@ -0,0 +1,18 @@
+import { Client } from '@hubspot/api-client';
+
+export class ImportsAwareMixinV3 {
+  clientV3: Client;
+  connectToV3: () => Promise<void>;
+
+  getImportDetails = async (id) => {
+    // Get a complete summary of an import record
+    // GET /crm/v3/imports/:importId
+    // https://developers.hubspot.com/docs/api/crm/imports
+
+    await this.connectToV3();
+    console.log('Calling hubspotClient.crm.imports.coreApi.getById. Retrieve a import details by id:', id);
+    const importResponse = await this.clientV3.crm.imports.coreApi.getById(id);
+    console.log('Import Response:', importResponse);
+    return importResponse;
+  }
+}
