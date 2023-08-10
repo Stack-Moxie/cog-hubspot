72b511586b938bc76b30babbe5b8a94752999917
Sev Geraskin <geraskin@gmail.com>
2023-08-10 15:28:07 -0700

    adding file metadata

--- src/client/mixins/imports-aware.ts
+++ src/client/mixins/imports-aware.ts
@@ -9,10 +9,9 @@ export class ImportsAwareMixinV3 {
     // GET /crm/v3/imports/:importId
     // https://developers.hubspot.com/docs/api/crm/imports
 
-    await this.connectToV3();
-    console.log('Calling hubspotClient.crm.imports.coreApi.getById. Retrieve a import details by id:', id);
-
     try {
+      await this.connectToV3();
+      console.log('Calling hubspotClient.crm.imports.coreApi.getById. Retrieve a import details by id:', id);
       const importResponse = await this.clientV3.crm.imports.coreApi.getById(id);
       console.log('Import Response:', importResponse);
       return importResponse;
@@ -26,17 +25,45 @@ export class ImportsAwareMixinV3 {
     // POST /crm/v3/imports
     // https://developers.hubspot.com/docs/api/crm/imports
 
-    await this.connectToV3();
-    console.log('Calling hubspotClient.crm.imports.coreApi.create. Create or Update a new import:');
+    const fileName = `imports-${new Date().toISOString()}.csv`;
+    // converting CSV to Buffer so we can send data as a file
     const file = {
       data: Buffer.from(fileBuffer, 'utf8'),
-      name: `imports-${new Date().toISOString()}.csv`,
+      name: fileName,
     };
 
-    // TODO: create file metadata
-    const fileMetadata = {};
+    // Create the column mappings array based on the given columnsToProperties
+    const columnMapArray = Object.keys(columnsToProperties).map((column) => {
+      return {
+        columnObjectTypeId: '0-1', // 0-1 is the object type id for contacts
+        columnName: column,
+        propertyName: columnsToProperties[column],
+
+        // specify the identifier on the front end
+        columnType: column.toLowerCase().includes(idColumn) ? 'HUBSPOT_ALTERNATE_ID' : undefined, // Special case for the ID column
+      };
+    });
+
+  // Construct the fileMetadata object
+    const fileMetadata = {
+      name: fileName,
+      importOperations: { '0-1': 'UPSERT' },
+      dateFormat: 'DAY_MONTH_YEAR',
+      files: [
+        {
+          fileName,
+          fileFormat: 'CSV',
+          fileImportPage: {
+            hasHeader: true,
+            columnMappings: columnMapArray,
+          },
+        },
+      ],
+    };
 
     try {
+      await this.connectToV3();
+      console.log('Calling hubspotClient.crm.imports.coreApi.create. Create or Update a new import:');
       const importResponse = await this.clientV3.crm.imports.coreApi.create(file, JSON.stringify(fileMetadata));
       console.log('Import Response:', importResponse);
       return importResponse;
